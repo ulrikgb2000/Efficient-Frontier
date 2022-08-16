61859f657160774d9d42e4c46b48bb1e75fc8d78
Herman <hermanvest@gmail.com>
2022-08-16 21:47:53 +0200

    going to be easier to gain info about portfolios

--- EfficientFT.py
+++ EfficientFT.py
@@ -1,11 +1,9 @@
 # https://www.youtube.com/watch?v=mJTrQfzr0R4
+# https://www.youtube.com/watch?v=f2BCmQBCwDs
+from statistics import mean
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
-
-df = yf.download(['TSLA', 'CAT'], start="2010-12-01", end="2022-06-01")
-
-df = np.log(1+df['Adj Close'].pct_change())
 
 def portfolioreturn (weights):
     return np.dot(df.mean(), weights)
@@ -20,6 +18,42 @@ def generateWeights(df):
     rand /= rand.sum()
     return rand
 
+tickers = ['ENTRA.OL', 'TOM', 'TSLA', 'CAT']
+potentialPortfolios = 10000
+
+df = yf.download(tickers, start="2010-12-01", end="2022-06-01")
+df = np.log(1+df['Adj Close'].pct_change())
+#print(df)
+
+weight = np.zeros((potentialPortfolios, len(tickers)))
+expectedReturn = np.zeros((potentialPortfolios, len(tickers)))
+expectedVolatility = np.zeros((potentialPortfolios, len(tickers)))
+sharpeRatio = np.zeros((potentialPortfolios, len(tickers)))
+
+meanLogReturn = df.mean(axis=0)
+covar = df.cov()
+
+for portfolio in range(potentialPortfolios):
+    # generate random weight vector
+    w = generateWeights(df)
+    weight[portfolio,:] = w
+    # expected log return
+    expectedReturn[portfolio] = np.sum(meanLogReturn * w)
+    # expected volatility
+    expectedVolatility[portfolio] = np.sqrt( np.dot(w.T, np.dot(covar, w)))
+    # sharpe ratio
+    sharpeRatio[portfolio] = expectedReturn[portfolio] / expectedVolatility[portfolio]
+
+bestSharpeIndex = sharpeRatio.argmax()
+#print(weight[bestSharpeIndex])
+print(len(sharpeRatio))
+plt.figure(figsize=(12,6))
+plt.scatter(expectedVolatility, expectedReturn)
+plt.xlabel("Expected volatility")
+plt.ylabel("Expected return")
+plt.show()
+
+"""
 pfReturns = []
 stds = []
 w = []
@@ -42,3 +76,4 @@ plt.show()
 
 
 min(stds)
+"""
